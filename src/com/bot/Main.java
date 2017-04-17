81d9e2e6b338682e90f8200bc906745b2d44e60a
vedoa.18 <lesyaved@mail.ru>
2017-04-17 10:54:39 +0300

    well start

--- src/com/bot/Main.java
+++ src/com/bot/Main.java
@@ -34,9 +34,15 @@ public class Main {
         JLabel Y = new JLabel("Y:");
         Y.setBounds(45,25,15,25);
         butPanel.add(Y);
+
+        JLabel X1 = new JLabel("X1:");
+        X1.setBounds(10,250,90,25);
+        butPanel.add(X1);
+
         JLabel N = new JLabel("NUM:");
         N.setBounds(2,70,30,25);
         butPanel.add(N);
+
         final JTextField x = new JTextField();
         x.setBounds(17,25, 25,25);
         butPanel.add(x);
@@ -46,6 +52,26 @@ public class Main {
         final JTextField n = new JTextField();
         n.setBounds(35,70,25,25);
         butPanel.add(n);
+        final JTextField x1 = new JTextField();
+        x1.setBounds(100,250,25,25);
+        butPanel.add(x1);
+        final JTextField y1 = new JTextField();
+        y1.setBounds(35,70,25,25);
+        butPanel.add(y1);
+        final JTextField x2 = new JTextField();
+        x2.setBounds(35,70,25,25);
+        butPanel.add(x2);
+        final JTextField x3 = new JTextField();
+        x3.setBounds(35,70,25,25);
+        butPanel.add(x3);
+        final JTextField y2 = new JTextField();
+        y2.setBounds(35,70,25,25);
+        butPanel.add(y2);
+        final JTextField y3 = new JTextField();
+        y3.setBounds(35,70,25,25);
+        butPanel.add(y3);
+
+
 
 
 
@@ -104,8 +130,23 @@ public class Main {
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-    }
 
+        JButton button3 = new JButton("Добавить треугольник");
+        button3.setBounds(2,200,180,40);
+        butPanel.add(button3);
+        button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Добавлен треугольник");
+                System.out.println("Точка 1: x1="+x1.getText()+"  y1="+y1.getText());
+
+                System.out.println("Точка 2: x2="+x2.getText()+"  y2="+y2.getText());
+                System.out.println("Точка 3: x3="+x3.getText()+"  y3="+y3.getText());
+
+
+            }
+        });
+    }
 
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
